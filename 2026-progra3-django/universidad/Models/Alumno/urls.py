fcaf1afcde777a0168d274ae1f7f58702ffcf28e
Jerelynm <jmarinm4@miumg.edu.gt>
2026-03-20 21:18:36 -0600

    se crea el crud para alumnos

--- 2026-progra3-django/universidad/Models/Alumno/urls.py
+++ 2026-progra3-django/universidad/Models/Alumno/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'alumno'
 
 urlpatterns = [
-    path('',                     views.alumno_list,   name='list'),
-    path('crear/',               views.alumno_create, name='create'),
-    path('<int:pk>/',            views.alumno_detail, name='detail'),
-    path('<int:pk>/editar/',     views.alumno_edit,   name='edit'),
-    path('<int:pk>/eliminar/',   views.alumno_delete, name='delete'),
+    path('', views.alumno_list, name='list'),
+    path('nuevo/', views.alumno_create, name='create'),
+    path('<int:pk>/', views.alumno_detail, name='detail'),
+    path('editar/<int:pk>/', views.alumno_edit, name='update'),
+    path('eliminar/<int:pk>/', views.alumno_delete, name='delete'),
 ]
